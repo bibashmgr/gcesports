d0988937ee5cf8de16a453f93d41f2b5d66d3927
bibashmgr <bibashthapamagar21@gmail.com>
2021-04-16 12:09:00 +0545

    Making some changes

--- src/main/controllers/feedback.php
+++ src/main/controllers/feedback.php
@@ -1,24 +1,14 @@
 <?php
 
-    // connecting to mysql server
-    $con = mysqli_connect('localhost', 'root');
+    require('../db/connect.php');
 
-    if($con){
-        echo "CONNECTION SUCCESSFUL";
-    }else {
-        echo "CONNECTION FAILED";
-    }
-
-    // connecting to database
-    mysqli_select_db($con, 'gcesports');
-
-    $email = $_POST['feedback-email'];
+    $feedbackEmail = $_POST['feedback-email'];
     $comments = $_POST['feedback-comments'];
 
-    $query = " INSERT INTO feedback(Email, Comments) VALUES('$email', '$comments') ";
+    $query = " INSERT INTO feedback(Email, Comments) VALUES('$feedbackEmail', '$comments') ";
 
     // inserting the inputed data into database
-    mysqli_query($con, $query);
+    mysqli_query($conn, $query);
 
     if (isset($_SERVER["HTTP_REFERER"])) {
         header("Location: " . $_SERVER["HTTP_REFERER"]);
