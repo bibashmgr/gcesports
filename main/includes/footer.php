8176acea245f5ce4b5ffd94ab1a1a0b74173b540
bibashmgr <bibashthapamagar21@gmail.com>
2021-11-10 15:33:23 +0545

    Updating repo

--- main/includes/footer.php
+++ main/includes/footer.php
@@ -7,46 +7,46 @@
             <div class="about-us-heading">
                 ABOUT US
             </div>
-            </div class="about-us-info">
-                A site that keeps updating news and moments<br/> 
-                of sport meet events held by Gandaki College <br/>
-                Of Engineering And Science(GCES) every year.
-            <div>
+        </div class="about-us-info">
+        A site that keeps updating news and moments<br />
+        of sport meet events held by Gandaki College <br />
+        Of Engineering And Science(GCES) every year.
+        <div>
             <div class="about-us-logo">
 
             </div>
         </div>
 
     </div>
-    
+
     <!-- footer: send-feedback section -->
     <div class="footer-send-msg">
 
         <div class="footer-send-msg-heading">
-                SEND FEEDBACK
+            SEND FEEDBACK
         </div>
         <form action="./main/controllers/feedback.php" method="POST" id="footer-feedback">
-                <label class="label-email">EMAIL:</label>
-                <input type="email" name="email" id="footer-email" placeholder="Enter Email" autocomplete="off"/>
-                <br/>
-                <label class="label-comments">COMMENTS:</label>
-                <textarea name="comments" id="footer-comments" placeholder="Enter comments" cols="22.5" rows="4" style="resize:vertical"></textarea>
-                <br/>
-                <input type="submit" value="SUBMIT" id="footer-submit-btn" name="comment-btn" />
+            <label class="label-email">EMAIL:</label>
+            <input type="email" name="email" id="footer-email" placeholder="Enter Email" autocomplete="off" />
+            <br />
+            <label class="label-comments">COMMENTS:</label>
+            <textarea name="comments" id="footer-comments" placeholder="Enter comments" cols="22.5" rows="4" style="resize:vertical"></textarea>
+            <br />
+            <input type="submit" value="SUBMIT" id="footer-submit-btn" name="comment-btn" />
         </form>
 
     </div>
 
     <!-- footer: contact-us section -->
     <div class="footer-contact">
-        
-        <div class="footer-contact-heading" >
+
+        <div class="footer-contact-heading">
             CONTACT
         </div>
-         <div class="footer-contact-info">
+        <div class="footer-contact-info">
             <i class="fas fa-envelope"></i>
             <span style="padding: 0rem 0.5rem;"></span>
-            be2018se637@gces.edu.np
+            be2018se635@gces.edu.np
         </div>
         <div class="footer-contact-info">
             <i class="fas fa-phone-alt"></i>
@@ -58,7 +58,7 @@
             <span style="padding: 0rem 0.5rem;"></span>
             Lamachour, Pokhara, Nepal
         </div>
-        
+
     </div>
 
 </footer>
